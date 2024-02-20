c3b4e24ec2d8568bddf43bcfb619d2822738282e
ayelabilal <ayelabilal944@gmail.com>
2024-02-20 17:10:46 +0500

    assignment continue

---
+++ app.js
@@ -0,0 +1,121 @@
+      // chapter no 1 "ALERTS"
+// Q:1
+ var a = "Assalam o alikum"
+ alert (a);
+
+//  Q:2
+var b = "Error!PLease enter a valid password"
+alert(b)
+
+// Q:3
+var c = "Welcom to JS Land.."
+var d = "Happy Coding!"
+alert ("welcom to JS land.. \n Happy Coding!")
+
+// Qno:4
+var e="Welcom to JS Land.."
+alert(e)
+
+var f = "Happy Coding"
+alert(f)
+
+// Qno:5
+var g ="Hello..I can run JS through my web browser's Console"
+alert(g)
+
+  // Chapter no 2 "VARIABLE FOR STRINGS"  
+            
+//   Q:1
+ var username ="ayela"
+ document.write(username);
+
+// Q:2
+var myName=" Ayela Imtiaz"
+document.write(myName);
+
+// Q:3
+var message= "Hello World"
+alert(message);
+
+// Q:4
+var nam ="Jhone Doe"
+var age = "15" 
+var course = "Certified Mobile Application Development"
+alert (nam);
+alert(age +"years old");
+alert(course);
+
+// Q:5
+var one = "PIZZA"
+var two = "PIZZ"
+var three = "PIZ"
+var four = "PI"
+var five = "P"
+alert("pizza \n pizz \n piz \n pi \n p")
+
+
+// Q:6
+var email=" email adress"
+alert(" my"+ email + " is example example.com");
+
+// Q:7
+var book= " A Smarter way to learn JavaScript"
+alert("I am trying to learn from the Book"+ book);
+
+// Q:8
+var text= " Yah! I can write HTML content through JavaScript"
+document.write(text);
+
+       // Chapter no 3 "VARIABLES FOR NUMBERS"
+
+// Q:1
+var age=" 24"
+alert("i am "+age + " years old");
+
+// Q:2
+var visit="14 times"
+alert("you have visited this site"+ visit);
+
+// Q:3
+var birth=" 1999"
+document.write(" My birth year is"+ birth);
+
+// Q:4
+var visitorname="John Doe"
+var product =" ordered"
+var quantity=" 5 T-shirts"
+
+document.write(visitorname+product+quantity+ " on XYZ clothing store");
+
+// WORD
+// chapter no 1 (ALERT)
+
+// q:1
+var firstname=" ayela"
+var lastname=" imtiaz"
+var email=" ayelaimtiaz@gmail.com"
+var phonenumber=" 123456789"
+var password=" xyz"
+ alert("first name:"+firstname);
+ alert( "last name:"+lastname);
+ alert("email:"+ email);
+ alert("phone number:"+phonenumber);
+ alert("password:"+ password);
+
+// q:3
+var message=" Never giveup"
+alert("message:"+message)
+
+// chapter no 2 (variables for string)
+
+// 1
+
+
+
+
+
+ 
+
+
+
+
